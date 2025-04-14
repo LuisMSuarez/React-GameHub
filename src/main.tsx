57a40d467a8cacbf921b1334a351d1e0bf8cb76c
Luis M. Suarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-04-14 09:55:36 -0700

    Implement color mode switch (#5)

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider themes={["light", "dark"]} defaultTheme="dark">
+    <Provider>
       <App />
     </Provider>
   </StrictMode>

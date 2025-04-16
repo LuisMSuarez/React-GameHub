0fdbe5ddd0315f3fd132394639313172822d5637
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-04-16 16:20:58 -0700

    platform names

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,11 +2,17 @@ import apiClient from "@/services/api-client";
 import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 
+interface Platforms {
+    id: number;
+    name: string;
+    slug: string;
+}
 export interface Game {
   id: number;
   name: string;
   background_image: string;
   rating: number;
+  parent_platforms: { platform: Platforms} []
 }
 
 interface FetchGamesResponse {

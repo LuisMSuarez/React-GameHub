3cf5117598cfe44bf81aeb5c560d31289898edf9
Luis M. Suarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-04-17 09:22:26 -0700

    Platform icons (#9)
    * platform names
    
    * Platform icon implementation
    
    * Added tooltip and accessibility name for platform
    
    * fixed grid layout
    
    * fallback in case of undefined platform

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,52 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone, MdOutlineDeviceUnknown } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs"; // for "web" games
+import { Platform } from "@/hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import { Tooltip } from "@/components/ui/tooltip";
+
+interface Props {
+  platforms: Platform[];
+}
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    android: FaAndroid,
+  };
+
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Tooltip content={platform.name}>
+          <Icon
+            as={
+              iconMap[platform.slug] !== undefined
+                ? iconMap[platform.slug]
+                : MdOutlineDeviceUnknown
+            }
+            color="gray.500"
+            aria-label={platform.name}
+          />
+        </Tooltip>
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;

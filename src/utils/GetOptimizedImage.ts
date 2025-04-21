f68e16f424b496f30f44e98c2fa46b7847c69bdc
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-04-21 12:22:23 -0700

    fixed special case for optimized images

--- src/utils/GetOptimizedImage.ts
+++ src/utils/GetOptimizedImage.ts
@@ -1,7 +1,16 @@
 import { useBreakpointValue } from "@chakra-ui/react";
 
 const getOptimizedImage = (image_uri: string) => {
-  const rootIndex = image_uri.indexOf("/games/");
+  let rootIndex = image_uri.indexOf("/games/");
+  // most images have a path like: https://media.rawg.io/media/games/618/618c2031a07bbff6b4f611f10b6bcdbc.jpg
+  // and crop as: https://media.rawg.io/media/crop/600/400/games/618/618c2031a07bbff6b4f611f10b6bcdbc.jpg
+  if (rootIndex == -1)
+  {
+    // some images have this path: https://media.rawg.io/media/screenshots/6fe/6fe228662a253cd929cc78a103541ee0.jpg
+    // they crop as: https://media.rawg.io/media/crop/600/400/screenshots/6fe/6fe228662a253cd929cc78a103541ee0.jpg
+    rootIndex = image_uri.indexOf("/screenshots/");
+  }
+  
   const rootUri = image_uri.substring(0, rootIndex);
   const suffix = image_uri.substring(rootIndex, image_uri.length);
   const optimizedUri = rootUri + "/crop/600/400" + suffix;

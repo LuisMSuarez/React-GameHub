853a4265cd541ded4b45bdde23c28a281de09ef7
Luis M. Suarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-04-24 12:44:02 -0700

    static data for genres and platforms (#29)

---
+++ src/data/platforms.ts
@@ -0,0 +1,77 @@
+// Copy of platform from results field of api call to: https://api.rawg.io/api/platforms/lists/parents
+// this data rarely changes, so we can hardcode it to avoid a network call
+// note that the original payload includes a platforms array for each child platform which I stripped out as it is not used
+
+export default
+[
+    {
+        "id": 1,
+        "name": "PC",
+        "slug": "pc",
+    },
+    {
+        "id": 2,
+        "name": "PlayStation",
+        "slug": "playstation",
+    },
+    {
+        "id": 3,
+        "name": "Xbox",
+        "slug": "xbox",
+    },
+    {
+        "id": 4,
+        "name": "iOS",
+        "slug": "ios",
+    },
+    {
+        "id": 8,
+        "name": "Android",
+        "slug": "android",
+    },
+    {
+        "id": 5,
+        "name": "Apple Macintosh",
+        "slug": "mac",
+    },
+    {
+        "id": 6,
+        "name": "Linux",
+        "slug": "linux",
+    },
+    {
+        "id": 7,
+        "name": "Nintendo",
+        "slug": "nintendo",
+    },
+    {
+        "id": 9,
+        "name": "Atari",
+        "slug": "atari",
+    },
+    {
+        "id": 10,
+        "name": "Commodore / Amiga",
+        "slug": "commodore-amiga",
+    },
+    {
+        "id": 11,
+        "name": "SEGA",
+        "slug": "sega",
+    },
+    {
+        "id": 12,
+        "name": "3DO",
+        "slug": "3do",
+    },
+    {
+        "id": 13,
+        "name": "Neo Geo",
+        "slug": "neo-geo",
+    },
+    {
+        "id": 14,
+        "name": "Web",
+        "slug": "web",
+    }
+]

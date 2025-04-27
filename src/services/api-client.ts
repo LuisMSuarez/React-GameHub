de659dddd82abc0d98e91643259c4141697be901
Luis M. Suarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-04-26 22:09:31 -0700

    Added prod endpoint (#32)

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,16 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "https://api.rawg.io/api",
+    /* uncomment to invoke rawg api directly from the client 
+    baseURL: "https://api.rawg.io/api", */
+    /* uncomment to invoke local api service from the client
+     baseURL: "https://localhost:7285/v1", */
+     /* production url */
+    baseURL: "https://gamers-hub-api.azurewebsites.net/v1",
+    /*
+    uncomment if using rawg api directly
     params: {
         key: import.meta.env.VITE_RAWG_API_KEY
     }
+    */
 })

6661eb5a74a0e6bada80e847dc41882c4a6728ff
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-04-28 12:12:00 -0700

    branding changes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
+import logo from "../assets/logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchBar from "./SearchBar";
 
@@ -11,7 +11,7 @@ interface Props {
 const NavBar = ({ searchString, onSearch }: Props) => {
   return (
     <HStack justifyContent="space-between" padding="10px" spaceX="20px">
-      <Image src={logo} boxSize="60px" />
+      <Image src={logo} boxSize="60px" borderRadius={6} />
       <SearchBar searchString={searchString} onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>

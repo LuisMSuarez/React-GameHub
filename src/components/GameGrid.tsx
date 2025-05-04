e3962ca09d499dc0e19f86b782ac1f93cd0e5195
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-05-03 21:48:28 -0700

    GamePagination component

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,6 +3,7 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import { GameQuery } from "@/App";
+import GamePagination from "./GamePagination";
 
 interface Props {
   gameQuery: GameQuery;
@@ -19,12 +20,15 @@ const GameGrid = ({ gameQuery }: Props) => {
   }
 
   return (
-    <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} gap={5} margin={5}>
-      {isLoading &&
-        skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
-      {!isLoading &&
-        games.map((game) => <GameCard key={game.id} game={game} />)}
-    </SimpleGrid>
+    <>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} gap={5} margin={5}>
+        {isLoading &&
+          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+        {!isLoading &&
+          games.map((game) => <GameCard key={game.id} game={game} />)}
+      </SimpleGrid>
+      <GamePagination />
+    </>
   );
 };
 

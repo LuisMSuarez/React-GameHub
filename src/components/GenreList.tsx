95cbd845679a7c413388fe465336e7a08d5d0101
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-05-14 11:10:49 -0700

    Genre selection clearing

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,12 @@
 import useGenres, { Genre } from "@/hooks/useGenres";
-import { VStack, Spinner, Heading } from "@chakra-ui/react";
+import { VStack, Spinner, Heading, HStack } from "@chakra-ui/react";
+import { Tooltip } from "@/components/ui/tooltip";
 import GenreItem from "./GenreItem";
+import { MdClose } from "react-icons/md";
 
 interface Props {
   selectedGenre: Genre | null;
-  onGenreSelect: (selectedGenre: Genre) => void;
+  onGenreSelect: (selectedGenre: Genre | null) => void;
 }
 
 const GenreList = ({ selectedGenre, onGenreSelect }: Props) => {
@@ -17,7 +19,14 @@ const GenreList = ({ selectedGenre, onGenreSelect }: Props) => {
   return (
     <VStack align="start" margin={3}>
       {isLoading && <Spinner size="xl" />}
-      <Heading marginBottom={3}>Genres</Heading>
+      <HStack marginBottom={3}>
+        <Heading>Genres</Heading>
+        {selectedGenre && (
+          <Tooltip content="Clear genre filter">
+            <MdClose onClick={() => onGenreSelect(null)} />
+          </Tooltip>
+        )}
+      </HStack>
       {genres?.map((genre) => (
         <GenreItem
           key={genre.slug}

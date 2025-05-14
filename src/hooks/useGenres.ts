7332dcfcfee4d7cc79457c1465156d37f24a0ed6
Luis M. Suarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-05-14 10:08:44 -0700

    Revert to previous genres behavior (#50)

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,4 @@
 import genres from "@/data/genres";
-import apiClient, { FetchDataResponse } from "@/services/api-client";
-import { useQuery } from "@tanstack/react-query";
 
 export interface Genre {
   id: number;
@@ -9,26 +7,6 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenres = () => 
-  useQuery<Genre[], Error>({
-      queryKey: ["genres"],
-      queryFn: () => 
-        apiClient
-          .get<FetchDataResponse<Genre>>("/genres")
-          .then((res) => res.data.results),
-      staleTime: Infinity,
-      refetchOnWindowFocus: false, // disable unwanted trigger
-      refetchInterval: (query) =>  { 
-        // the strategy is to start off by loading static data with the
-        // initialData property and then using an interval, check for new
-        // data one time in the background
-        // this way, the user will never see a loading spinner
-        // if we never fetched the data (ie: render from cache)
-        // then we force a refetch immediately
-        // otherwise we never refetch 
-        return (query.state.dataUpdateCount > 0) ? Infinity: 1;
-        },
-      initialData: genres // initialize cache with static data to show results to the user immediately
-  })
+const useGenres = () => ({ data : genres, error: "", isLoading: false });
 
 export default useGenres;

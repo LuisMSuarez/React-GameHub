6caf80a7a8760e47f31d55b193f7503c3b70a5db
Luis M. Suarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-05-29 11:48:13 -0700

    Game detail page work and navigation fixes (#62)
    * Link on logo
    
    * Navigate to homepage on search
    
    * better handling of 404 and 403 errors
    
    * game image links to detail page
    
    * touchups

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,14 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchBar from "./SearchBar";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" margin="10px" spaceX="0px">
-      <Image src={logo} boxSize="60px" borderRadius={6} />
+      <Link to="/">
+        <Image src={logo} boxSize="60px" borderRadius={6} />
+      </Link>
       <SearchBar />
       <ColorModeSwitch />
     </HStack>

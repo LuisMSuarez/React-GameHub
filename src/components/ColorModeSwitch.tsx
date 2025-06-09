400d21ea1bed7298a8847e0946fdc1a40591a50b
Luis M. Suarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-06-09 12:28:40 -0700

    Color mode icon (#71)
    * Color mode switch with icon
    
    * remove console log

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,17 +1,25 @@
-import { HStack, Switch } from "@chakra-ui/react";
+import { HStack, Icon, Switch } from "@chakra-ui/react";
 import { useColorMode } from "@/components/ui/color-mode";
+import { FaMoon } from "react-icons/fa";
+import { FaSun } from "react-icons/fa";
 
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
   return (
     <HStack>
       <Switch.Root
+        colorPalette="blue"
+        size="lg"
         checked={colorMode === "dark"}
         onCheckedChange={toggleColorMode}
       >
         <Switch.HiddenInput />
-        <Switch.Control />
-        <Switch.Label whiteSpace="nowrap">Dark Mode</Switch.Label>
+        <Switch.Control>
+          <Switch.Thumb />
+          <Switch.Indicator fallback={<Icon as={FaMoon} color="gray.400" />}>
+            <Icon as={FaSun} color="yellow.400" />
+          </Switch.Indicator>
+        </Switch.Control>
       </Switch.Root>
     </HStack>
   );

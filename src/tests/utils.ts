9142d0ff805a085e7dddcd269047c08be1d5663b
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-09-29 12:21:26 -0700

    Increase timeout

--- src/tests/utils.ts
+++ src/tests/utils.ts
@@ -3,7 +3,7 @@ import { By, until, WebDriver } from "selenium-webdriver";
 export async function waitAndClick(
   driver: WebDriver,
   selector: By,
-  timeout = 10000
+  timeout = 30000
 ) {
   const el = await driver.wait(until.elementLocated(selector), timeout); // element in the DOM
   await driver.wait(until.elementIsVisible(el), timeout / 2); // element fully rendered & visible

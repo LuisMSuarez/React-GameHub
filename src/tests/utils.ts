0c3ae7b66788240ebf3da6a0825ed7d1252d97aa
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-09-29 12:39:13 -0700

    Fixes

--- src/tests/utils.ts
+++ src/tests/utils.ts
@@ -3,7 +3,7 @@ import { By, until, WebDriver } from "selenium-webdriver";
 export async function waitAndClick(
   driver: WebDriver,
   selector: By,
-  timeout = 30000
+  timeout = 10000
 ) {
   const el = await driver.wait(until.elementLocated(selector), timeout); // element in the DOM
   await driver.wait(until.elementIsVisible(el), timeout / 2); // element fully rendered & visible

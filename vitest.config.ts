1a7abdd448d438590c7fbbb14e0be8a6cc120726
Luis M. Suarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-11-17 13:09:48 -0800

    Chunking of build (#105)

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from "vitest/config";
 import path from "path";
 
 export default defineConfig({
-    resolve: {
-        alias: {
-          "@": path.resolve(__dirname, "./src"),
-        },
-      },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",

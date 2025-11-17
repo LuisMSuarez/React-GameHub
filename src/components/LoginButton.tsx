b130d3ef3837e8cb089a1a28fd8250a8cadee9a7
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-11-17 14:42:54 -0800

    Display avatar when user is logged in

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import { useMsal } from "@azure/msal-react";
 import { loginRequest } from "../auth/authConfig";
 import { Button } from "@chakra-ui/react";
+import { Avatar } from "@chakra-ui/react";
+import { MdLogin } from "react-icons/md";
+import { Tooltip } from "@/components/ui/tooltip";
 
 export const LoginButton: React.FC = () => {
-  const { instance } = useMsal();
+  const { instance, accounts } = useMsal();
 
   if (import.meta.env.VITE_FEATURE_AUTH !== "enabled") return null;
 
@@ -17,5 +20,24 @@ export const LoginButton: React.FC = () => {
     }
   };
 
-  return <Button onClick={handleLogin}>Login</Button>;
+  if (accounts.length === 0) {
+    return (
+      <Button
+        variant="plain"
+        size="md"
+        borderRadius="full"
+        onClick={handleLogin}
+      >
+        <MdLogin />
+      </Button>
+    );
+  } else {
+    return (
+      <Avatar.Root>
+        <Tooltip content={accounts[0].name}>
+          <Avatar.Fallback name={accounts[0].name} />
+        </Tooltip>
+      </Avatar.Root>
+    );
+  }
 };
